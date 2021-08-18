0b94e339f7f0cde82e8b48f23504d609f75a7fd0
Vaidehi <Vdarji@confederationcollege.ca>
2021-08-18 12:49:32 -0400

    payload  list added

--- CP380-B1-BlockList/Models/Payload.cs
+++ CP380-B1-BlockList/Models/Payload.cs
@@ -8,6 +8,21 @@ namespace CP380_B1_BlockList.Models
 
     public class Payload
     {
-        // TODO
+        public string user
+        { get; set; }
+        public TransactionTypes transType
+        { get; set; }
+        public string titem
+        { get; set; }
+        public int tamount
+        { get; set; }
+
+        public Payload(string userName, TransactionTypes transType,  string titem , int tamount)
+        {
+            this.user = userName;
+            this.transType = transType;
+            this.titem = titem;
+            this.tamount = tamount;
+        }
     }
 }
