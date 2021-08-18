2b6601c37fde68eab5c0685b727748d55a470351
Vaidehi <Vdarji@confederationcollege.ca>
2021-08-18 16:18:44 -0400

    block

--- CP380-B1-BlockList/Models/Block.cs
+++ CP380-B1-BlockList/Models/Block.cs
@@ -33,10 +33,8 @@ namespace CP380_B1_BlockList.Models
             var sha256 = SHA256.Create();
             var json = JsonSerializer.Serialize(Data);
 
-            //
-            // TODO
-            //
-            var inputString = $""; // TODO
+          
+            var inputString = $"{TimeStamp.Date:yyyy-MM-dd hh:mm:ss tt}-{PreviousHash ?? ""}-{Nonce}-{Data}";
 
             var inputBytes = Encoding.ASCII.GetBytes(inputString);
             var outputBytes = sha256.ComputeHash(inputBytes);
@@ -46,7 +44,9 @@ namespace CP380_B1_BlockList.Models
 
         public void Mine(int difficulty)
         {
-            // TODO
+           
+            
+            }
         }
     }
 }
